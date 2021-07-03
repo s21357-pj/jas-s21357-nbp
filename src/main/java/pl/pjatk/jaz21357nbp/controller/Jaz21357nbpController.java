e3a0b94621e819a3e807c2fcbf6f16702d8d579a
Ilya R <s21357@pjwstk.edu.pl>
2021-07-03 14:45:41 +0200

    swagger done

--- src/main/java/pl/pjatk/jaz21357nbp/controller/Jaz21357nbpController.java
+++ src/main/java/pl/pjatk/jaz21357nbp/controller/Jaz21357nbpController.java
@@ -1,5 +1,6 @@
 package pl.pjatk.jaz21357nbp.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +13,10 @@ public class Jaz21357nbpController {
     public Jaz21357nbpController(Jaz21357nbpService jaz21357nbpService) {
         this.jaz21357nbpService = jaz21357nbpService;
     }
-
+    @ApiOperation(value = "calculate average currency rate for the time period",
+            response = Double.class,
+            notes = "This method will return average currency rate for the time period or error code"
+    )
     @GetMapping("/avg/{currency}/{from}/{to}")
     public ResponseEntity<Double> getAvg(@PathVariable String currency, @PathVariable String from, @PathVariable String to) {
         return new ResponseEntity<>(jaz21357nbpService.getAvg(from, to, currency), HttpStatus.OK);
