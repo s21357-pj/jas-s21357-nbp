803e1d9172646bb7519fcf104e8eafeb7bc30cc4
Ilya R <s21357@pjwstk.edu.pl>
2021-07-03 15:00:39 +0200

    clean imports

--- src/main/java/pl/pjatk/jaz21357nbp/GlobalExceptionHandler.java
+++ src/main/java/pl/pjatk/jaz21357nbp/GlobalExceptionHandler.java
@@ -1,12 +1,11 @@
 package pl.pjatk.jaz21357nbp;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
+
 
 import java.net.ConnectException;
 
